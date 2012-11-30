259cd97872c396ba2966a8271015f301bc41724f
Tyler Long <tyler4long@gmail.com>
2012-11-30 18:17:20 +0800

    Initial commit

---
+++ ruby/rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YoudaoBlog::Application.config.secret_token = 'f48515e8884a8b21c4ba01dd5c86d0861d09cd41e6be8168c8808e5876bcdaa9ab9692cb20a542ecd464ef94686ddc28fd21ae6de5bdf1d1bfa19ea1fd2e8318'

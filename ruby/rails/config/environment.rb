ce7fb05d3b6725aec80448c2a5c51794aa5923ba
Tyler Long <tyler4long@gmail.com>
2012-12-01 12:09:26 +0800

    Add main controller

--- ruby/rails/config/environment.rb
+++ ruby/rails/config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Load global configurations
+CONFIG = Hashie::Mash.new YAML.load_file(Rails.root.join("config", "application.yml"))[Rails.env]
+
 # Initialize the rails application
 YoudaoBlog::Application.initialize!
